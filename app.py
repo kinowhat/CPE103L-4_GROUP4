f65b615d3714122e13671b9464df14d389d3c788
kinowhat <shitakemush1990@gmail.com>
2026-03-11 02:01:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import sqlite3
 
@@ -18,40 +18,16 @@ def setup_db():
     conn.commit()
     conn.close()
 
-setup_db()  
+setup_db()
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-@app.route('/login', methods=['POST'])
-def login():
-    data = request.get_json()
-    username = data['username']
-    password = data['password']
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT role FROM users WHERE username=? AND password=?", (username, password))
-    user = cursor.fetchone()
-    conn.close()
-    if user:
-        return {'role': user[0]}
-    else:
-        return {'role': None}
+@app.route('/admin')
+def admin_page():
+    return render_template('admin.html')
 
-@app.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()
-    name = data['name']
-    email = data['email']
-    password = data['password']
-    try:
-        conn = connect_db()
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO users (username, password, role) VALUES (?, ?, 'user')", (email, password))
-        conn.commit()
-        conn.close()
-        return {'message': 'Registered successfully!'}
-    except sqlite3.IntegrityError:
-        return {'message': 'Email already exists!'}
-    
 @app.route('/add_item', methods=['POST'])
 def add_item():
     data = request.get_json()
@@ -59,22 +35,24 @@ def add_item():
     price = data['price']
     qty = data['qty']
     image = data['image']
-    try:
-        conn = connect_db()
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO items (name, price, qty, image) VALUES (?, ?, ?, ?)", (name, price, qty, image))
-        conn.commit()
-        conn.close()
-        return {'message': 'Registered successfully!'}
-    except sqlite3.IntegrityError:
-        return {'message': 'Item already exists!'}
+
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO items (name, price, qty, image) VALUES (?, ?, ?, ?)",
+        (name, price, qty, image)
+    )
+    conn.commit()
+    conn.close()
+
+    return jsonify({'message': 'Item added successfully!'})
 
 @app.route('/get_items', methods=['GET'])
 def get_items():
     conn = connect_db()
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM items")
-    rows = cursor.fetchall()  
+    rows = cursor.fetchall()
     conn.close()
 
     items = []
@@ -84,11 +62,10 @@ def get_items():
             "name": row[1],
             "price": row[2],
             "qty": row[3],
-            "image": row[4],
+            "image": row[4]
         })
+
     return jsonify({"items": items})
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
